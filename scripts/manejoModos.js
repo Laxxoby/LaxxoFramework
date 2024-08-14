601c4bd440076d109e9029b595bad2b49fcc6cf3
Johan Lasso <laxxoby.j7@gmail.com>
2024-08-14 16:54:03 -0500

    botton neon

--- scripts/manejoModos.js
+++ scripts/manejoModos.js
@@ -4,6 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const icon = toggleButton.querySelector("i");
     const html = document.documentElement;
 
+    //Neon
+    const toggleBtnNeon = document.getElementById("neon-toggle");
+    const iconNeon = toggleBtnNeon.querySelector("i");
+
+    const currentNeon = localStorage.getItem("neon") || "off";
+    html.setAttribute("neon", currentNeon);
+    iconNeon.classList.add(currentNeon === "off" ? "fa-bolt" : "fa-droplet");
+
     // Cargar el tema guardado en el almacenamiento local
     const currentTheme = localStorage.getItem("theme") || "light";
     html.setAttribute("theme-mode", currentTheme);
@@ -20,4 +28,17 @@ document.addEventListener("DOMContentLoaded", function () {
             localStorage.setItem("theme", "dark");
         }
     });
+
+    toggleBtnNeon.addEventListener('click', () => {
+        if (html.getAttribute("neon") === "off") {
+            html.setAttribute("neon", "on");
+            iconNeon.classList.replace("fa-bolt", "fa-droplet");
+            localStorage.setItem("neon", "on");
+        } else {
+            html.setAttribute("neon", "off");
+            iconNeon.classList.replace("fa-droplet", "fa-bolt");
+            localStorage.setItem("neon", "off");
+        }
+    });
+
 });
